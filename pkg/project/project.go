6d0015d6f39c095411a3ab094aeb6e0cbd0b129a
GitHub Action <action@github.com>
2020-06-22 12:20:39 +0000

    pkg/project: bump version to 1.0.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "template-operator"
 	source      = "https://github.com/giantswarm/template-operator"
-	version     = "1.0.0"
+	version     = "1.0.1-dev"
 )
 
 func Description() string {
